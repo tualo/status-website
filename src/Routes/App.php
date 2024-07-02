02b71a97464360391d19fc21b400d3b731a3c994
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-07-02 10:25:43 +0200

    add regions to the app routes

--- src/Routes/App.php
+++ src/Routes/App.php
@@ -9,10 +9,25 @@ use Tualo\Office\StatusWebsite\State as S;
 
 class App implements IRoute{
     public static function register(){
+
+        BasicRoute::add('/status-website-app/regions',function($matches){
+            try{
+                TApp::contenttype('application/json');
+                $export = DSTable::instance('status_website_workflow_regions')->read()->get();
+
+                TApp::result('data',$export);
+                TApp::result('success',true);
+                
+            }catch(\Exception $e){
+                TApp::result('msg', $e->getMessage());
+            }
+        },['get'],true);
+
         BasicRoute::add('/status-website-app/getworkflows',function($matches){
             try{
                 TApp::contenttype('application/json');
                 $export = DSTable::instance('status_website_workflows')->read()->get();
+                
 
                 foreach($export as $index => $item){
                     
